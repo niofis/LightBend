44f4372effd3ea6375c7760dc92007947d43be7c
niofis <niofis@gmail.com>
2012-02-07 01:34:58 -0700

    Added PEZ as GLUT replacement.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
-//
-//  main.cpp
-//  LightBend
-//
-//  Created by Enrique Carrillo Rubio on 31/01/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #include <iostream>
+#include <stdlib.h>
+#include "pez.h"
+#include <glew.h>
+#include <math.h>
 
-int main (int argc, const char * argv[])
+static float elapsed = 0;
+static int pressing = 0;
+static float speed = 0.01f;
+
+const char* PezInitialize(int width, int height)
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    return "Pez Intro";
 }
 
+void PezRender()
+{
+    if (pressing) {
+        glClearColor(1, 1, 0.75f, 1);
+    } else {
+        float blue = 0.5f * (1.0f + sinf(elapsed));
+        glClearColor(0, 0.25f, blue, 1);
+    }
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void PezUpdate(unsigned int elapsedMilliseconds)
+{
+    elapsed += elapsedMilliseconds * speed;
+}
+
+void PezHandleMouse(int x, int y, int action)
+{
+    if (action == PEZ_DOWN)
+        pressing = 1;
+    else if (action == PEZ_UP)
+        pressing = 0;
+}
